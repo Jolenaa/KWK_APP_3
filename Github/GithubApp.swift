a894efb7f8b2cb12c08c446b236dd517f45888a6
Jolena Akudago <Jolkudago@gmail.com>
2023-06-12 09:37:42 -0600

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Jolena Akudago on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
